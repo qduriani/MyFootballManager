bf31501036a6a44adc8255bfb41b6a95cee8e22c
Quentin Duriani <qduriani@gmail.com>
2023-10-15 15:44:52 +0200

    Implement circuit breaker

--- playerservice/src/main/java/com/myfootballmanager/playerservice/controller/PlayerServiceController.java
+++ playerservice/src/main/java/com/myfootballmanager/playerservice/controller/PlayerServiceController.java
@@ -2,60 +2,103 @@ package com.myfootballmanager.playerservice.controller;
 
 import java.util.Map;
 import java.util.ArrayList;
+import java.time.Duration;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HttpMethod;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
 import com.myfootballmanager.playerservice.model.Player;
 
 @RestController
 public class PlayerServiceController {
-
+  
+  private static final CircuitBreaker circuitBreaker;
+  
+  static {
+    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+    .failureRateThreshold(50)
+    .waitDurationInOpenState(Duration.ofMillis(1000))
+    .slidingWindowSize(2)
+    .build();
+    
+    circuitBreaker = CircuitBreaker.of("player-service", circuitBreakerConfig);
+  }
+  
   static ArrayList<Player> players = new ArrayList<Player>() {{
     add(new Player(0, "Benji", "Bastia"));
     add(new Player(1, "FPF", "Bastia"));
     add(new Player(2, "2J", "ACA GAULOIS"));
     add(new Player(3, "JBay", "Monaco"));
   }};
-
-  @GetMapping("/players/{id}")
+  
+  // @GetMapping("/players/{id}")
   public String get(@PathVariable Integer id) {
-    Player player = players.stream().filter(p -> p.id == id).findFirst().get();
+    Player player = players.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     return player.toString();
   }
-
-  @PostMapping("/players")
+  
+  // @PostMapping("/players")
   public String post(@RequestBody Map<String, String> body) {
-    if (!(body.containsKey("name") && body.containsKey(("team_name")))) {
-      return "Error: the fields <name> and <team_name> needs to be specified";
+    if (!(body.containsKey("name") && body.containsKey("team_name"))) {
+      throw new RuntimeException("Error: the fields <name> and <team_name> needs to be specified");
     }
     Player player = new Player(players.size(), body.get("name"), body.get("team_name"));
     players.add(player);
     return player.toString();
   }
-
-  @PutMapping("/players/{id}")
+  
+  // @PutMapping("/players/{id}")
   public String put(@PathVariable Integer id, @RequestBody Map<String, String> body) {
-    if (!(body.containsKey("name") && body.containsKey(("team_name")))) {
-      return "Error: the fields <name> and <team_name> needs to be specified";
+    if (!(body.containsKey("name") && body.containsKey("team_name"))) {
+      throw new RuntimeException("Error: the fields <name> and <team_name> needs to be specified");
     }
-    Player player = players.stream().filter(t -> t.id == id).findFirst().get();
+    Player player = players.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     player.name = body.get("name");
     player.team_name = body.get("team_name");
     players.set(id, player);
     return player.toString();
   }
-
-  @DeleteMapping("/players/{id}")
+  
+  // @DeleteMapping("/players/{id}")
   public String delete(@PathVariable Integer id) {
-    Player player = players.stream().filter(t -> t.id == id).findFirst().get();
+    Player player = players.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     Boolean status = players.remove(player);
     return "Delete: " + status;
   }
 
+  @PostMapping("/players")
+  public String handle_post(@RequestBody Map<String, String> body) {
+    return circuitBreaker.executeSupplier(() -> post(body));
+  }
+  
+  @RequestMapping("/players/{id}")
+  public String getplayer(HttpServletRequest request, @PathVariable Integer id, @RequestBody Map<String, String> body) {
+    try {
+      switch (request.getMethod()) {
+      case HttpMethod.GET:
+        return circuitBreaker.executeSupplier(() -> get(id));
+      case HttpMethod.PUT:
+        return circuitBreaker.executeSupplier(() -> put(id, body));
+      case HttpMethod.DELETE:
+        return circuitBreaker.executeSupplier(() -> delete(id));
+      default:
+        return fallback("Method not allowed");
+      }
+    } catch (Exception e) {
+      return fallback(e.getMessage());
+    }
+  }
+  
+  private String fallback(String message) {
+    return message;
+  }
+  
 }
--- teamservice/src/main/java/com/myfootballmanager/teamservice/controller/TeamServiceController.java
+++ teamservice/src/main/java/com/myfootballmanager/teamservice/controller/TeamServiceController.java
@@ -2,58 +2,101 @@ package com.myfootballmanager.teamservice.controller;
 
 import java.util.Map;
 import java.util.ArrayList;
+import java.time.Duration;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HttpMethod;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 
 import com.myfootballmanager.teamservice.model.Team;
 
 @RestController
 public class TeamServiceController {
-
+  
+  private static final CircuitBreaker circuitBreaker;
+  
+  static {
+    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+    .failureRateThreshold(50)
+    .waitDurationInOpenState(Duration.ofMillis(1000))
+    .slidingWindowSize(2)
+    .build();
+    
+    circuitBreaker = CircuitBreaker.of("team-service", circuitBreakerConfig);
+  }
+  
   static ArrayList<Team> teams = new ArrayList<Team>() {{
     add(new Team(0, "Bastia"));
     add(new Team(1, "ACA GAULOIS"));
     add(new Team(2, "Monaco"));
   }};
-
-  @GetMapping("/teams/{id}")
+  
+  // @GetMapping("/teams/{id}")
   public String get(@PathVariable Integer id) {
-    Team team = teams.stream().filter(t -> t.id == id).findFirst().get();
+    Team team = teams.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     return team.toString();
   }
-
-  @PostMapping("/teams")
+  
+  // @PostMapping("/teams")
   public String post(@RequestBody Map<String, String> body) {
-    if (!body.containsKey("name")) {
-      return "Error: the field <name> needs to be specified";
+    if (!(body.containsKey("name"))) {
+      throw new RuntimeException("Error: the field <name> needs to be specified");
     }
     Team team = new Team(teams.size(), body.get("name"));
     teams.add(team);
     return team.toString();
   }
-
-  @PutMapping("/teams/{id}")
+  
+  // @PutMapping("/teams/{id}")
   public String put(@PathVariable Integer id, @RequestBody Map<String, String> body) {
-    if (!body.containsKey("name")) {
-      return "Error: the field <name> needs to be specified";
+    if (!(body.containsKey("name"))) {
+      throw new RuntimeException("Error: the field <name> needs to be specified");
     }
-    Team team = teams.stream().filter(t -> t.id == id).findFirst().get();
+    Team team = teams.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     team.name = body.get("name");
     teams.set(id, team);
     return team.toString();
   }
-
-  @DeleteMapping("/teams/{id}")
+  
+  // @DeleteMapping("/teams/{id}")
   public String delete(@PathVariable Integer id) {
-    Team team = teams.stream().filter(t -> t.id == id).findFirst().get();
+    Team team = teams.stream().filter(p -> p.id == id).findFirst().orElseThrow();
     Boolean status = teams.remove(team);
     return "Delete: " + status;
   }
 
+  @PostMapping("/teams")
+  public String handle_post(@RequestBody Map<String, String> body) {
+    return circuitBreaker.executeSupplier(() -> post(body));
+  }
+  
+  @RequestMapping("/teams/{id}")
+  public String getteam(HttpServletRequest request, @PathVariable Integer id, @RequestBody Map<String, String> body) {
+    try {
+      switch (request.getMethod()) {
+      case HttpMethod.GET:
+        return circuitBreaker.executeSupplier(() -> get(id));
+      case HttpMethod.PUT:
+        return circuitBreaker.executeSupplier(() -> put(id, body));
+      case HttpMethod.DELETE:
+        return circuitBreaker.executeSupplier(() -> delete(id));
+      default:
+        return fallback("Method not allowed");
+      }
+    } catch (Exception e) {
+      return fallback(e.getMessage());
+    }
+  }
+  
+  private String fallback(String message) {
+    return message;
+  }
+  
 }
